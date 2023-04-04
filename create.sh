1b37310498f382f15134540458b869f4de9d3760
jamieplu <179417684+jamieplu@users.noreply.github.com>
2023-04-04 12:03:39 -0500

    create my daily diary 04th

--- create.sh
+++ create.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-for day in {15..31}
+year=2023
+
+for i in {1..31}
 do
+    # Generate a random day between 1 and 366 (accounting for leap years)
+    random_day=$(( ( RANDOM % 366 )  + 1 ))
+
+    # Convert the random day number to a date (adjusted format)
+    date_str=$(date -j -v +$((random_day - 1))d +'%Y-%m-%d')
+
+    # Extract day number from the date
+    day=$(date -j -f '%Y-%m-%d' "$date_str" +'%d')
+
     suffix="th"
     if [[ $day == 1 || $day == 21 || $day == 31 ]]; then
         suffix="st"
@@ -10,7 +21,7 @@ do
     elif [[ $day == 3 || $day == 23 ]]; then
         suffix="rd"
     fi
-    echo ${day}${suffix}dy > "day_${day}${suffix}.txt"
+    touch "day_${day}${suffix}.txt"
 
    #git rm "day_${day}${suffix}.txt"
     git add *

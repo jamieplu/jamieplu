f9fd8ef4045e02215339cf0eed4e9a1d718dec6b
jamieplu <179417684+jamieplu@users.noreply.github.com>
2024-06-02 11:42:08 -0500

    bugfixes 4

--- create.sh
+++ create.sh
@@ -2,7 +2,7 @@
 
 for i in {4..10}
 do
-    touch "ferociously-blogging_$i.txt"
+    touch "as-is-blog_$i.md"
     git add *
-    git commit --date "Mar 2$st, 2024" -m "bugfixes $i"
+    git commit --date "Jun 2$st, 2024" -m "bugfixes $i"
 done
